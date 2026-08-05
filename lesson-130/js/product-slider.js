2a7fb36bb14004f5358b491e6b5c4d6d045d1aa4
CrowVirginForever <144515457+CrowVirginForever@users.noreply.github.com>
2026-08-05 10:20:52 +1000

    Add files via upload

---
+++ lesson-130/js/product-slider.js
@@ -0,0 +1,16 @@
+export const productSlider = () => {
+    new Swiper(".product__slider", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        loop: true,
+
+        mousewheel: {
+            forceToAxis: true,
+        },
+
+        navigation: {
+            nextEl: ".product__slider-button--next",
+            prevEl: ".product__slider-button--prev",
+        },
+    });
+};
