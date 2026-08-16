90f7dc4e8b4339de8659ed14c339bfd0a25883ff
CrowVirginForever <144515457+CrowVirginForever@users.noreply.github.com>
2026-08-16 11:55:24 +1000

    Add files via upload

---
+++ js-10/main.js
@@ -0,0 +1,23 @@
+// Задача 1
+let firstName = "Ваня"
+let lastName = "Тракторов"
+let isStudent = true
+
+// Задача 2
+let age = 20
+currentYear = 2026
+let birthYear = currentYear - age
+console.log(birthYear)
+
+// Задача 3
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`)
+
+// Задача 4
+let a = '123'
+let b = +'456'
+let c = Number('789')
+let d = Boolean(0)
+let e = Boolean(' ')
+let results = a + b + c + d + e
+
+console.log(results)
