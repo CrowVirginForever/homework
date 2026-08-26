b997e152cb880a4df477bc4c78f7ae2e22d44678
CrowVirginForever <144515457+CrowVirginForever@users.noreply.github.com>
2026-08-26 20:59:19 +1000

    Add files via upload

---
+++ js-52/main.js
@@ -0,0 +1,64 @@
+// Задача 1
+const users = [
+    { name: 'Alex', age: 24, isAdmin: false },
+    { name: 'Bob', age: 13, isAdmin: false },
+    { name: 'John', age: 31, isAdmin: true },
+    { name: 'Jane', age: 20, isAdmin: false },
+]
+
+users.push(
+    { name: 'Anne', age: 19, isAdmin: false },
+    { name: 'Jack', age: 43, isAdmin: true },
+)
+
+console.log(users)
+
+// Задача 2
+let ageSum = 0
+
+const getUsersAverageAge = (array) => {
+    array.forEach((object) => {
+        ageSum += object.age
+    })
+    return ageSum / array.length
+}
+
+console.log(getUsersAverageAge(users))
+
+// Задача 3
+const adminsArray = []
+
+const getAllAdmins = (array) => {
+    array.forEach((object) => {
+        if (object.isAdmin) {
+            adminsArray.push(object)
+        }
+    })
+    return adminsArray
+}
+
+console.log(getAllAdmins(users))
+
+// Задача 4
+
+const first = (arr, n) => {
+    const first_n_array = []
+
+    if (n <= arr.length) {
+        if (n == 0) {
+        } else if (n == undefined) {
+            first_n_array.push(arr[0])
+        } else {
+            for (let i = 0; i < n; i++) {
+                first_n_array.push(arr[i])
+            }
+        }
+        return first_n_array
+    } 
+
+    console.error(
+    `${n} - больше длины массива!`)
+    return 
+}
+
+console.log(first(users, 2))
